4ea162ce9d5bf4b96b29b6b6fab85074e2d17cd2
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-04-04 23:19:26 -0400

    Add files via upload

---
+++ StateCapitals2.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+    -Create a HashMap to hold the bames of all the states and their corresponding capital names.
+    -Load the HashMap with each state/cpaital pair using the file given
+    -
+
+
+
+*/
+
+public class StateCapitals2 {
+    
+    //private String Name;
+    private Scanner input = new Scanner(System.in);
+
+
+
+    public static void main(String[] args){
+
+        Capital capital = new Capital();
+        HashMap <String, Capital> StateCapitals = new HashMap<>();
+
+
+        System.out.println("50 STATE & CAPITALS ARE LOADED");
+        System.out.prinln();
+        
+
+
+    }
+
+    
+
+
+    
+}
